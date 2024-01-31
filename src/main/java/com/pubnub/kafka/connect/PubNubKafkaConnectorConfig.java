3e210c0f4f970d856d1c3f084d9006ca6676668b
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-01-31 14:07:35 +0100

    Code cleanup, proper shutdown, dead-letter-queue support

--- src/main/java/com/pubnub/kafka/connect/PubNubKafkaConnectorConfig.java
+++ src/main/java/com/pubnub/kafka/connect/PubNubKafkaConnectorConfig.java
@@ -1,11 +1,11 @@
 package com.pubnub.kafka.connect;
 
-import java.util.Map;
-
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
+
+import java.util.Map;
 
 public class PubNubKafkaConnectorConfig extends AbstractConfig {
 
@@ -14,6 +14,7 @@ public class PubNubKafkaConnectorConfig extends AbstractConfig {
     }
 
     public static final ConfigDef CONFIG_DEF = createConfigDef();
+
     private static ConfigDef createConfigDef() {
         ConfigDef configDef = new ConfigDef();
         addParams(configDef);
@@ -22,32 +23,31 @@ public class PubNubKafkaConnectorConfig extends AbstractConfig {
 
     private static void addParams(final ConfigDef configDef) {
         configDef.define(
-            "pubnub.subscribe_key",
-            Type.STRING,
-            Importance.HIGH,
-            "The PubNub Subscribe API KEY")
-        .define(
-            "pubnub.publish_key",
-            Type.STRING,
-            Importance.HIGH,
-            "The PubNub Publish API KEY")
-        .define(
-            "pubnub.secret_key",
-            Type.STRING,
-            Importance.HIGH,
-            "The PubNub Secret API KEY")
-        .define(
-            "task.sleep.timeout",
-            Type.INT,
-            5000,
-            Importance.HIGH,
-            "Sleep timeout used by tasks during each poll")
-        .define(
-            "monitor.thread.timeout",
-            Type.INT,
-            10000,
-            Importance.LOW,
-            "Timeout used by the monitoring thread");
+                        "pubnub.user_id",
+                        Type.STRING,
+                        Importance.HIGH,
+                        "The PubNub User ID")
+                .define(
+                        "pubnub.subscribe_key",
+                        Type.STRING,
+                        Importance.HIGH,
+                        "The PubNub Subscribe API KEY")
+                .define(
+                        "pubnub.publish_key",
+                        Type.STRING,
+                        Importance.HIGH,
+                        "The PubNub Publish API KEY")
+                .define(
+                        "pubnub.secret_key",
+                        Type.STRING,
+                        Importance.HIGH,
+                        "The PubNub Secret API KEY")
+                .define(
+                        "task.sleep.timeout",
+                        Type.INT,
+                        5000,
+                        Importance.HIGH,
+                        "Sleep timeout used by tasks during each poll");
     }
 
 }
